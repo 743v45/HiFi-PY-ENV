e222dfd06ca5e6c113fab161fb3686475ddbb826
yu.zheng <yu.zheng@upai.com>
2016-09-12 11:31:23 +0800

    Add install.sh for pip and supervisor

---
+++ install.sh
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+#    author    :   丁雪峰
+#    time      :   2016-09-12 02:19:51
+#    email     :   fengidri@yeah.net
+#    version   :   1.0.1
+
+
+
+./python/bin/python2.7 ./python/bin/get-pip.py
+./python/bin/pip2 install supervisor
